d0e450b34e65d39bbfdf542ba54b1078ff0fd642
Vishall0317 <vishalbramhankar18@gmail.com>
2024-03-30 12:30:11 +0530

    initial commit

---
+++ product-service/src/main/java/org/ebazaar/Product.java
@@ -0,0 +1,11 @@
+package org.ebazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Product{
+    public static void main(String[] args) {
+        SpringApplication.run(Product.class, args);
+    }
+}
